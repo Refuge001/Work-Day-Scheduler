e2eafb400dc951f01ddeb76484e071f26973a301
Will Utterback <uwill08@gmail.com>
2020-12-27 17:23:45 -0600

    Starting the local storage learning session

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 $('#currentDay').text(moment().format('MMMM DD, YYYY'));
 
 // Thanks Tish!
-$('.timecolor').each(function () {
+$('.hour').each(function () {
   var currentHour = (moment().hour());
   var blockTime = parseInt($(this).attr('id'));
   if (currentHour === blockTime) {
-    $(this.nextElementSibling).addClass('present');
+    $(this).next().addClass('present');
   } else if (currentHour < blockTime) {
-    $(this.nextElementSibling).addClass('future');
+    $(this).next().addClass('future');
   } else if (currentHour > blockTime) {
-    $(this.nextElementSibling).addClass('past');
+    $(this).next().addClass('past');
   }
 }
 );
+
+function storageSave() {
+  var description = $(this).prev().val();
+  console.log(description);
+  console.log(this);
+  console.log($(this).prev());
+  localStorage.setItem('Description', description);
+};
+
+
+$('.btn').on('click', storageSave);
