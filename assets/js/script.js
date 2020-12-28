d154545d9904494611338c4782064c80f330742b
Will Utterback <uwill08@gmail.com>
2020-12-27 20:38:50 -0600

    Implemented localStorage code infrastructure and the iterative process of loading to schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,23 @@ $('#currentDay').text(moment().format('MMMM DD, YYYY'));
 
 // Thanks Tish!
 $('.hour').each(function () {
-  var currentHour = (moment().hour());
   var blockTime = parseInt($(this).attr('id'));
+  var currentHour = (moment().hour());
+  var description = localStorage.getItem(blockTime);
   if (currentHour === blockTime) {
-    $(this).next().addClass('present');
+    $(this).next().addClass('present').text(description);
   } else if (currentHour < blockTime) {
-    $(this).next().addClass('future');
+    $(this).next().addClass('future').text(description);
   } else if (currentHour > blockTime) {
-    $(this).next().addClass('past');
+    $(this).next().addClass('past').text(description);
   }
 }
 );
 
 function storageSave() {
   var description = $(this).prev().val();
-  console.log(description);
-  console.log(this);
-  console.log($(this).prev());
-  localStorage.setItem('Description', description);
+  var pH = $(this).prev().prev().attr('id');
+  localStorage.setItem(pH, description);
 };
-
 
 $('.btn').on('click', storageSave);
